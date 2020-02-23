a66d4bb29bc7a0ddc64758732ba3276ca8ff934b
autonopy <57545485+autonopy@users.noreply.github.com>
2020-02-23 11:44:28 -0500

    Uploading sanitized password file

---
+++ FourSquare_login.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 14 11:34:46 2020
+
+@author: drken
+"""
+CLIENT_ID = 'MYIDHERE' # your Foursquare ID
+CLIENT_SECRET = 'MYSECRETHERE' # your Foursquare Secret
