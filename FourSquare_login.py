6c8d6a34632f1b5169cf41a0a98d0fcd95081641
autonopy <57545485+autonopy@users.noreply.github.com>
2020-02-23 12:49:39 -0500

    moved to subfolder

--- FourSquare_login.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 14 11:34:46 2020
-
-@author: drken
-"""
-CLIENT_ID = 'MYIDHERE' # your Foursquare ID
-CLIENT_SECRET = 'MYSECRETHERE' # your Foursquare Secret
